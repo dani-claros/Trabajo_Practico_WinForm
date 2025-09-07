ccf0aeace62a31821cecfeaef83b150c87df9a1b
Dana <clarosdana03@gmail.com>
2025-09-07 14:49:37 -0300

    Se creo class Articulo

---
+++ Programacion/AppGestion/Articulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppGestion
+{
+    class Articulo
+    {
+        public int CodigoArticulo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal Precio { get; set; }
+
+        //Imagen class Imagen
+        //Marca class Marca
+        //Categoria class Categoria
+
+    }
+}
